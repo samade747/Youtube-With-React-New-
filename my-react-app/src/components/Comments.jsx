205bf01ebfd4d2e6d92c137a8e032de3a6eda17b
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-04-16 02:04:36 +0500

    204pm 16 4 2024

--- my-react-app/src/components/Comments.jsx
+++ my-react-app/src/components/Comments.jsx
@@ -2,9 +2,34 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
- 
+    display: flex;  
+    gap: 10px;
+    margin: 30px 0px;
 
 `;
+
+const NewComment = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+`;
+
+const Avatar = styled.img`
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+`;
+
+const Input = styled.input`
+    border: none;
+    border-bottom: 1px solid ${({theme}) => theme.soft};
+    background-color: transparent;
+    outline: none;
+    padding: 5px;
+    width: 100%;
+     
+`
 
 
 
@@ -13,9 +38,12 @@ const Comments = () => {
 
     return (
         <Container>
+            <NewComment>
 
+                <Avatar src="../img/avatar.png" />  
+                <Input placeholder="Add a comment..." />
 
-            Comments
+            </NewComment>           
         </Container>
     )
 }
--- my-react-app/src/components/Video.jsx
+++ my-react-app/src/components/Video.jsx
@@ -162,6 +162,9 @@ const Video = () => {
             // 5142
         </channel>
         </Content>
+        <Hr />
+        <Comment />
+        
         <Recommmendation>recommndation</Recommmendation>
     </Container>;
 };
